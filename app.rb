eb5b183f36d85174743d0cdb1ed558c036e60ac9
Yurina Shin <yurinas@gmail.com>
2018-09-06 19:57:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    @session = session
+  end
   configure do
     enable :sessions
     set :session_secret, "secret"
-    
+
   end
 end
